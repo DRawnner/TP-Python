0d2662d1143df9a77019c32c2faf4c7005ef62df
DRawnner <DRawnner@gmail.com>
2020-01-27 22:32:48 +0000

    1

--- main.py
+++ main.py
@@ -12,36 +12,6 @@ def test():
 # --------------- ADICIONAR REGISTOS À TABELA ---------------------
 
 
-@app.route('/adduser', methods=['POST'])
-def add_user():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    try:
-        _json = request.json
-        _user_name = _json['user_name']
-        _user_email = _json['user_email']
-        _user_password = _json['user_password']
-
-        if _user_name and _user_email and _user_password and request.method == 'POST':
-            sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES (%s, %s, %s)"
-            data = (_user_name, _user_email, _user_password)
-            cursor.execute(sql, data)
-            conn.commit()
-            resp = jsonify('Sala adicionada com sucesso!')
-            resp.status_code = 200
-            return resp
-        else:
-            resp1 = jsonify('Ocorreu um erro ao adicionar a sala!')
-            return resp1
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
-        return 'Teste'
-
-
-
 @app.route('/addcasa', methods=['POST'])
 def add_casa():
     conn = mysql.connect()
@@ -70,40 +40,6 @@ def add_casa():
 
 
 # ------------------ VER TODOS OS REGISTOS DA TABELA ----------
-
-@app.route('/users')
-def users():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("SELECT * FROM tbl_user")
-        rows = cursor.fetchall()
-        resp = jsonify(rows)
-        resp.status_code = 200
-        return resp
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
-
-
-@app.route('/user/<int:id>')
-def user(id):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    try:
-
-        cursor.execute("SELECT * FROM tbl_user WHERE user_id=%s", id)
-        row = cursor.fetchone()
-        resp = jsonify(row)
-        resp.status_code = 200
-        return resp
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
 
 
 @app.route('/casas')
@@ -157,7 +93,7 @@ def update_casa():
         _garagem = _json['garagem']
         # validar valores recebidos
         if _num_divisoes and _garagem and _id_casa and request.method == 'POST':
-            sql = "UPDATE casa SET _num_divisoes=%s, garagem=%s WHERE id_casa=%s"
+            sql = "UPDATE casa SET num_divisoes=%s, garagem=%s WHERE id_casa=%s"
             data = (_num_divisoes, _garagem, _id_casa,)
 
             cursor.execute(sql, data)
@@ -175,7 +111,7 @@ def update_casa():
 
 # ----------------- APAGAR REGISTO ----------------------
 
-@app.route('/delete/<int:id>')
+@app.route('/deletecasa/<int:id>')
 def delete_casa(id):
     conn = mysql.connect()
     cursor = conn.cursor()
